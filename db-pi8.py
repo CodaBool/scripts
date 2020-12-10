98a0b6cbfa7f73c35d2a928f4446616a58f70660
codabool <codabool@pm.me>
2020-12-10 16:31:30 -0500

    setup for new pi

--- db-pi8.py
+++ db-pi8.py
@@ -16,15 +16,17 @@ def getExternalSpace():
   return 100 - int(newout.split()[10][:-1])
 
 def executeQuery(cursor, connection):
-  new = re.sub('b|\'|n|\\\\', '', str(subprocess.check_output("python /home/codabool/Downloads/codadash-scripts/count-simple.py /mnt/sd1/ven/jellyfin/new n", shell=True)))
-  # space = getSpace() # this is not as useful as external drive space, could use this if I add new column for it in db
-  externalSpace = getExternalSpace()
+  # new = re.sub('b|\'|n|\\\\', '', str(subprocess.check_output("python3 ~/scripts/count-simple.py /mnt/sd1/ven/jellyfin/new n", shell=True)))
+  space = getSpace() # this is not as useful as external drive space, could use this if I add new column for it in db
+  new = 3
+  # externalSpace = getExternalSpace()
   lastRan = datetime.now()
-  cursor.execute(f'UPDATE pi8 SET "Space Left"={externalSpace}, "New Videos"={new}, "Last Ran"=\'{lastRan}\' WHERE id=1;') # syntax requires > python 3.6
+  print(f'UPDATE pi8 SET "Space Left"={space}, "New Videos"={new}, "Last Ran"=\'{lastRan}\' WHERE id=1;')
+  cursor.execute(f'UPDATE pi8 SET "Space Left"={space}, "New Videos"={new}, "Last Ran"=\'{lastRan}\' WHERE id=1;') # syntax requires > python 3.6
 
 try:
   load_dotenv()
-  connection = psycopg2.connect(os.getenv('URI'))
+  connection = psycopg2.connect(os.getenv('URI_HEROKU'))
   cursor = connection.cursor()
   executeQuery(cursor, connection)
   connection.commit()
