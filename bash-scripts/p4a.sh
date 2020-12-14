6a20c64f78f7b0a7b1acb9de4b7b504e287c2f41
Coda Bool <CodaBool@pm.me>
2020-12-14 18:37:34 -0500

    asd

--- bash-scripts/p4a.sh
+++ bash-scripts/p4a.sh
@@ -8,8 +8,8 @@ printf "\n====================================================\n\n"
 systemctl is-active Jackett >/dev/null 2>&1 && printf "Jackett: ${GREEN}Running${NC}\n" || printf "Jackett: ${RED}Stopped${NC}\n"
 systemctl is-active sonarr >/dev/null 2>&1 && printf "Sonarr: ${GREEN}Running${NC}\n" || printf "Sonarr: ${RED}Stopped${NC}\n"
 systemctl is-active Radarr >/dev/null 2>&1 && printf "Radarr: ${GREEN}Running${NC}\n" || printf "Radarr: ${RED}Stopped${NC}\n"
-PROCESS_NUM='ps -ef | grep "qbittorrent" | grep -v "grep" | wc -l'
-if [ $PROCESS_NUM -eq 1 ]; then
+PROCESS_NUM=$(ps -ef | grep "qbittorrent" | grep -v "grep" | wc -l)
+if [ $PROCESS_NUM -gt 0 ]; then
   printf "Qbittorrent: ${GREEN}Running${NC}\n"
 else
   printf "Qbittorrent: ${RED}Stopped${NC}\n"
