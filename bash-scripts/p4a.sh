03dfa6512079b165b9255078cddf527bf1c8f808
Coda Bool <CodaBool@pm.me>
2020-12-14 18:20:09 -0500

    setup p4a script

---
+++ bash-scripts/p4a.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "\n====================================================\n\n"
+systemctl is-active Jackett >/dev/null 2>&1 && printf "Jackett: ${GREEN}Running${NC}\n" || printf "Jackett: ${RED}Stopped${NC}\n"
+systemctl is-active sonarr >/dev/null 2>&1 && printf "Sonarr: ${GREEN}Running${NC}\n" || printf "Sonarr: ${RED}Stopped${NC}\n"
+systemctl is-active Radarr >/dev/null 2>&1 && printf "Radarr: ${GREEN}Running${NC}\n" || printf "Radarr: ${RED}Stopped${NC}\n"
+systemctl is-active qbittorrent >/dev/null 2>&1 && printf "Radarr: ${GREEN}Running${NC}\n" || printf "Radarr: ${RED}Stopped${NC}\n"
+protonvpn status
+PROCESS_NUM='ps -ef | grep "qbittorrent" | grep -v "grep" | wc -l'
+if [ $PROCESS_NUM -eq 1 ]; then
+  printf "Qbittorrent: ${GREEN}Running${NC}\n"
+else
+  printf "Qbittorrent: ${RED}Stopped${NC}\n"
+fi
+printf "\n================ ...Happy Torrenting ===================\n\n"
+
