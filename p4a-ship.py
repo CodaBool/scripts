9796c12d6342affea59172cfc20067b6761ed44a
Coda Bool <CodaBool@pm.me>
2020-12-16 23:52:46 -0500

    as

--- p4a-ship.py
+++ p4a-ship.py
@@ -32,7 +32,7 @@ def moveFolder(listOfFiles):
       pathArr = _file.split("/")
       readyFolderPath = _file[:-8]
       readyFolderName = pathArr[-2:][0]
-      print("transfering ", videosFound, " videos to mom")
+      print("beginning transfer of", videosFound, "videos to mom")
       print('pathArr', pathArr)
       print('readyFolderPath', readyFolderPath)
       print('readyFolderName', readyFolderName)
@@ -40,10 +40,10 @@ def moveFolder(listOfFiles):
       
       print("\nSSH Copy of folder " + readyFolderName)
       print("DEBUG " + "sshpass -p " +  os.getenv('MOM_PASS') + " scp -r \"" + readyFolderPath + "\" " + SSH + ":/mnt/sd1/ven/media/new")
-      # os.system("sshpass -p " +  os.getenv('MOM_PASS') + " scp -r \"" + readyFolderPath + "\" " + SSH + ":/mnt/sd1/ven/media/new")
+      os.system("sshpass -p " +  os.getenv('MOM_PASS') + " scp -r \"" + readyFolderPath + "\" " + SSH + ":/mnt/sd1/ven/media/new")
       print("SSH Adding im.done file")
       print("DEBUG " + "sshpass -p " +  os.getenv('MOM_PASS') + " scp " + SCRIPTS_HOME + "im.done " + SSH + ":\'\"/mnt/sd1/ven/media/new/" + readyFolderName + "\"\'")
-      # os.system("sshpass -p " +  os.getenv('MOM_PASS') + " scp " + SCRIPTS_HOME + "im.done " + SSH + ":\'\"/mnt/sd1/ven/media/new/" + readyFolderName + "\"\'")
+      os.system("sshpass -p " +  os.getenv('MOM_PASS') + " scp " + SCRIPTS_HOME + "im.done " + SSH + ":\'\"/mnt/sd1/ven/media/new/" + readyFolderName + "\"\'")
       print("SSH Copy Complete\n\nRemoving shipment folder from docks")
       print("rm -rf \"" + readyFolderPath + "\"")
       # os.system("rm -rf \"" + readyFolderPath + "\"")
@@ -55,10 +55,12 @@ SCRIPTS_HOME = 'home/codabool/scripts/'
 load_dotenv()
 
 print('\n==================')
-if isfile(ROOT + "shipping.started"):
-  print("Shipment in progress try again later.\nTo force start remove " + ROOT + "shipping.started")
-else:
-  os.system("touch " + ROOT + "shipping.started")
-  moveFolder(getListOfFiles(ROOT))
-  os.system("rm " + ROOT + "shipping.started")
+moveFolder(getListOfFiles(ROOT))
+
+# if isfile(ROOT + "shipping.started"):
+#   print("Shipment in progress try again later.\nTo force start remove " + ROOT + "shipping.started")
+# else:
+#   os.system("touch " + ROOT + "shipping.started")
+#   moveFolder(getListOfFiles(ROOT))
+#   os.system("rm " + ROOT + "shipping.started")
 print('==================\n')
