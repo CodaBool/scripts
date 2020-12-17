a8a1254b83d9ed24153f9f7b174230fdde4ec5a6
Coda Bool <CodaBool@pm.me>
2020-12-17 03:11:43 -0500

    a

--- bash-scripts/minecraft.sh
+++ bash-scripts/minecraft.sh
@@ -16,12 +16,23 @@
 # fi
 
 # display last five lines of the file using tail command
-example = "[02:33:24] [Server thread/INFO]: CodaBool joined the game"
+# example="[02:33:24] [Server thread/INFO]: CodaBool joined the game"
 OUT=$(tail -1 /opt/minecraft/server/logs/latest.log)
-if [[ $example == *"joined the game"* ]]; then
-  echo "It's there!"
+if [[ $OUT == *"joined the game"* ]]; then
+  IP=$(curl https://g5rkignyck.execute-api.us-east-1.amazonaws.com)
+  echo "Someone has entered! using quote "$IP
+  /opt/minecraft/tools/mcrcon/mcrcon -p admin "say $IP"
+  touch /home/codabool/yes-findy
 else
-  echo "oh poo it's not there"
+  echo "oh poo I don't see anyone"
+  touch /home/codabool/no-findy
 fi
 
 #echo "output =" $OUT
+# str="Learn-to-Split-a-String-in-Bash-Scripting"
+# IFS='-' # hyphen (-) is set as delimiter
+# read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
+# for i in "${ADDR[@]}"; do # access each element of array
+#     echo "$i"
+# done
+# IFS=' ' # reset to default value after usage
