9442605c56172cd8b995be5402759cd1abdaf083
Coda Bool <CodaBool@pm.me>
2020-12-16 23:31:39 -0500

    start making p4a ship

--- p4a-ship.py
+++ p4a-ship.py
@@ -4,10 +4,6 @@ import subprocess
 from os.path import join, isdir, isfile
 from dotenv import load_dotenv
 
-# python /home/codabool/Downloads/codadash-scripts/ship-p4a.py /home/codabool/Downloads/ROOT/ \"n\"
-
-ROOT = "/docks/"
-
 def getListOfFiles(directory): # create a list of file and sub directories
   listOfFile = os.listdir(directory) # names in the given directory
   allFiles = list()
@@ -27,80 +23,71 @@ def countVideos(listOfFiles):
         totalVideos += 1
   return totalVideos
 
-def writeDestination(directory):
-  listOfFile = os.listdir(directory)
-  allRootDirs = list()
-  for entry in listOfFile:
-    fullPath = join(directory, entry)
-    name = entry
-    if isdir(fullPath):
-      allRootDirs.append(fullPath)
-    elif isfile(fullPath):
-      handleLooseFile(fullPath, name, os.path.splitext(name)[0])
-  for dirPath in allRootDirs:
-    print("Added ship.win to", dirPath)
-    os.system("touch \"" + dirPath  + "/ship.win\"")
-  
-def handleLooseFile(fullPath, name, nameNoExt):
-  print("Found Loose file at docks: ", fullPath, name, nameNoExt)
-  os.system("mkdir --parents \"" + ROOT  + nameNoExt + "\"; mv \"" + ROOT + name + "\" \"" + ROOT  + nameNoExt + "\"")
-
-def moveFolder(listOfFiles, auto):
+def moveFolder(listOfFiles):
   print(countVideos(listOfFiles), "videos found in", ROOT)
   for _file in listOfFiles:
-    if "ship.win" in _file:
+    if "ship.mom" in _file:
       # variables
       pathArr = _file.split("/")
+
+      # _file[7:]
       readyFolderPath = _file[:-8]
       readyFolderName = pathArr[-2:][0]
-      print(countVideos(getListOfFiles(readyFolderPath)), " videos found in this transfer to windows")
+      print(countVideos(getListOfFiles(readyFolderPath)), " videos found in this transfer to mom")
+      print('variables')
+      print('pathArr', pathArr)
+      print('readyFolderPath', readyFolderPath)
+      print('readyFolderName', readyFolderName)
+      print('os.getenv(MOM_PASS)', os.getenv(MOM_PASS))
+    else:
+      print('no shipment label found', _file)
+    #   if 'n' in auto:
+    #     response = input("\nContinue? [y/n] ")
+    #     if response == 'n':
+    #       os.system("rm /home/codabool/Documents/shipping.started")
+    #       sys.exit()
 
-      if 'n' in auto:
-        response = input("\nContinue? [y/n] ")
-        if response == 'n':
-          os.system("rm /home/codabool/Documents/shipping.started")
-          sys.exit()
+    #   print("\nSSH Copy of folder " + readyFolderName)
+    #   print("DEBUG sshpass -p " +  os.getenv('WIN_PASS') + " scp -r \"" + readyFolderPath + "\" Dougie@192.168.1.27:/Transcode/pull")
+    #   os.system("sshpass -p " +  os.getenv('WIN_PASS') + " scp -r \"" + readyFolderPath + "\" Dougie@192.168.1.27:/Transcode/pull")
+    #   print("SSH Adding im.done file")
+    #   print("DEBUG sshpass -p " +  os.getenv('WIN_PASS') + " scp /home/codabool/Documents/im.done Dougie@192.168.1.27:\'\"/Transcode/pull/" + readyFolderName + "\"\'")
 
-      load_dotenv()
-      print("\nSSH Copy of folder " + readyFolderName)
-      print("DEBUG sshpass -p " +  os.getenv('WIN_PASS') + " scp -r \"" + readyFolderPath + "\" Dougie@192.168.1.27:/Transcode/pull")
-      os.system("sshpass -p " +  os.getenv('WIN_PASS') + " scp -r \"" + readyFolderPath + "\" Dougie@192.168.1.27:/Transcode/pull")
-      print("SSH Adding im.done file")
-      print("DEBUG sshpass -p " +  os.getenv('WIN_PASS') + " scp /home/codabool/Documents/im.done Dougie@192.168.1.27:\'\"/Transcode/pull/" + readyFolderName + "\"\'")
+    #   os.system("sshpass -p " +  os.getenv('WIN_PASS') + " scp /home/codabool/Documents/im.done Dougie@192.168.1.27:\'\"/Transcode/pull/" + readyFolderName + "\"\'")
+    #   print("SSH Copy Complete\n\nRemoving shipment folder from docks")
+    #   print("rm -rf \"" + readyFolderPath + "\"")
+    #   # os.system("rm -rf \"" + readyFolderPath + "\"")
+    # elif "ship.pi8" in _file:
+    #   # variables
+    #   pathArr = _file.split("/")
+    #   readyFolderPath = _file[:-8]
+    #   readyFolderName = pathArr[-2:][0]
+    #   print(countVideos(getListOfFiles(readyFolderPath)), " videos found in this transfer to pi8")
 
-      os.system("sshpass -p " +  os.getenv('WIN_PASS') + " scp /home/codabool/Documents/im.done Dougie@192.168.1.27:\'\"/Transcode/pull/" + readyFolderName + "\"\'")
-      print("SSH Copy Complete\n\nRemoving shipment folder from docks")
-      print("rm -rf \"" + readyFolderPath + "\"")
-      # os.system("rm -rf \"" + readyFolderPath + "\"")
-    elif "ship.pi8" in _file:
-      # variables
-      pathArr = _file.split("/")
-      readyFolderPath = _file[:-8]
-      readyFolderName = pathArr[-2:][0]
-      print(countVideos(getListOfFiles(readyFolderPath)), " videos found in this transfer to pi8")
+    #   if 'n' in auto:
+    #     response = input("\nContinue? [y/n] ")
+    #     if response == 'n':
+    #       os.system("rm /home/codabool/Documents/shipping.started")
+    #       sys.exit()
 
-      if 'n' in auto:
-        response = input("\nContinue? [y/n] ")
-        if response == 'n':
-          os.system("rm /home/codabool/Documents/shipping.started")
-          sys.exit()
+      
+    #   print("\nSSH Copy of folder " + readyFolderName)
+    #   os.system("sshpass -p " +  os.getenv('PI8_PASS') + " scp -r \"" + readyFolderPath + "\" codabool@192.168.1.32:/mnt/sd1/ven/jellyfin/new")
+    #   print("SSH Adding im.done file")
+    #   os.system("sshpass -p " +  os.getenv('PI8_PASS') + " scp /home/codabool/Documents/im.done codabool@192.168.1.32:\'\"/mnt/sd1/ven/jellyfin/new/" + readyFolderName + "\"\'")
+    #   print("SSH Copy Complete\n\nRemoving shipment folder from docks")
+    #   print("rm -rf \"" + readyFolderPath + "\"")
+    #   os.system("rm -rf \"" + readyFolderPath + "\"")
 
-      load_dotenv()
-      print("\nSSH Copy of folder " + readyFolderName)
-      os.system("sshpass -p " +  os.getenv('PI8_PASS') + " scp -r \"" + readyFolderPath + "\" codabool@192.168.1.32:/mnt/sd1/ven/jellyfin/new")
-      print("SSH Adding im.done file")
-      os.system("sshpass -p " +  os.getenv('PI8_PASS') + " scp /home/codabool/Documents/im.done codabool@192.168.1.32:\'\"/mnt/sd1/ven/jellyfin/new/" + readyFolderName + "\"\'")
-      print("SSH Copy Complete\n\nRemoving shipment folder from docks")
-      print("rm -rf \"" + readyFolderPath + "\"")
-      os.system("rm -rf \"" + readyFolderPath + "\"")
+ROOT = "/docks/"
+TYPES = ['mp4', 'mkv', 'avi']
+load_dotenv()
 
-if isfile("/home/codabool/shipping.started"):
-  print("Shipment in progress try again later.\nTo force start remove ~/shipping.started")
+print('\n==================')
+if isfile(ROOT + "shipping.started"):
+  print("Shipment in progress try again later.\nTo force start remove " + ROOT + "shipping.started")
 else:
-  os.system("touch /home/codabool/shipping.started")
-  TYPES = ['mp4', 'mkv', 'avi']
-  directory = sys.argv[1]
-  auto = sys.argv[2]
-  # writeDestination(directory)
-  moveFolder(getListOfFiles(directory), auto)
-  os.system("rm /home/codabool/shipping.started")
+  os.system("touch " + ROOT + "shipping.started")
+  moveFolder(getListOfFiles(ROOT))
+  os.system("rm " + ROOT + "shipping.started")
+print('==================\n')
