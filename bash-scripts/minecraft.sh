c517c34b1630759bf97ac37e1d45ff4037dddd4d
Coda Bool <CodaBool@pm.me>
2020-12-17 02:22:18 -0500

    a

--- bash-scripts/minecraft.sh
+++ bash-scripts/minecraft.sh
@@ -6,4 +6,4 @@ IP=$(curl https://g5rkignyck.execute-api.us-east-1.amazonaws.com)
 echo "IP = $IP"
 # sed "s/IP/$IP/" nsupdate.txt | nsupdate
 # echo "sed =" $sed
-# /opt/minecraft/tools/mcrcon/mcrcon -p admin 
+/opt/minecraft/tools/mcrcon/mcrcon -p admin "'$IP'" 
