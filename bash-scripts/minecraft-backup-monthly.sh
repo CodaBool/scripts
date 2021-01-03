987c01028a6471fa98fe692a5d0b3cd8afe79d5a
Coda Bool <CodaBool@pm.me>
2021-01-02 21:56:38 -0500

    asd

--- bash-scripts/minecraft-backup-monthly.sh
+++ bash-scripts/minecraft-backup-monthly.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # ran using crontab every Month at 6am first day
 # MONTH = full text of the current month
-SOMETHING=$(LC_ALL=C date +%B + %Y)
-YEAR=date +"%Y"
-echo $SOMETHING
-echo $YEAR
-cmd_output=$(date +"%B + % Y")
-echo $cmd_output
+MONTH_YEAR=$(date +"%B + %Y")
+echo $MONTH_YEAR
 
 # rm -rf /opt/minecraft/backups/world-$MONTH
 # cp -r /opt/minecraft/server/world /opt/minecraft/backups/world-$MONTH
