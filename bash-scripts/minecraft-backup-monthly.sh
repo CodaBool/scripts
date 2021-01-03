e51e4682d37065a42e945b93a95cd296be61b03d
Coda Bool <CodaBool@pm.me>
2021-01-02 21:55:45 -0500

    test

--- bash-scripts/minecraft-backup-monthly.sh
+++ bash-scripts/minecraft-backup-monthly.sh
@@ -5,6 +5,9 @@ SOMETHING=$(LC_ALL=C date +%B + %Y)
 YEAR=date +"%Y"
 echo $SOMETHING
 echo $YEAR
+cmd_output=$(date +"%B + % Y")
+echo $cmd_output
+
 # rm -rf /opt/minecraft/backups/world-$MONTH
 # cp -r /opt/minecraft/server/world /opt/minecraft/backups/world-$MONTH
 # /opt/minecraft/tools/mcrcon/mcrcon -p admin "say Making a monthly backup of the world -Doggie Corp."
