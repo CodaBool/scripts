bc31f8790cfc3bb5ea7874ad84f416d9ebe3133d
Coda Bool <CodaBool@pm.me>
2021-01-02 21:27:11 -0500

    test

--- bash-scripts/minecraft-backup.sh
+++ bash-scripts/minecraft-backup.sh
@@ -4,3 +4,4 @@
 rm -rf /opt/minecraft/backups/old-world
 mv /opt/minecraft/backups/current-world /opt/minecraft/backups/old-world
 cp -r /opt/minecraft/server/world /opt/minecraft/backups/current-world
+/opt/minecraft/tools/mcrcon/mcrcon -p admin "say testing saying something -Doggie"
